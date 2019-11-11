ae414b092f39bcac25c3b4a392cae873e6adea72
JeffreyBool <1402992668@qq.com>
2019-11-11 20:35:14 +0800

    增加模型注释

--- app/Consts/const.php
+++ app/Consts/const.php
@@ -5,3 +5,19 @@
  * Date: 2019/11/11
  * Time: 20:26
  */
+
+// 分页配置常量
+const COMMON_DEFAULT_PAGE_SIZE = 10; // 默认每页数量
+const COMMON_MIN_PAGE_SIZE = 10; // 每页最小数量
+const COMMON_MAX_PAGE_SIZE = 30; // 每页最大数量
+
+// 通用数据状态
+const COMMON_STATUS_NORMAL = 1; // 正常
+const COMMON_STATUS_HIDDEN = 2; // 隐藏 or 停用/禁用
+const COMMON_STATUS_DELETED = 3; // 已删除
+
+
+// 通用 is_值
+const COMMON_TRUE = 1;
+const COMMON_FALSE = 0;
+
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -13,10 +13,47 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
+/**
+ * App\Models\Admin
+ *
+ * @property int $id
+ * @property string $name 用户名
+ * @property string $email 邮箱
+ * @property string $password 密码
+ * @property string|null $portrait 头像
+ * @property int $login_count 登录次数
+ * @property string $last_login_ip 最后登录IP
+ * @property int $status 状态，1正常 2禁止 3删除
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Client[] $clients
+ * @property-read int|null $clients_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Token[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereLoginCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin wherePortrait($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Admin whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Admin extends Authenticatable
 {
     use Notifiable, HasApiTokens;
 
+    /**
+     * @var array
+     */
     protected $fillable = [
         'name',
         'email',
