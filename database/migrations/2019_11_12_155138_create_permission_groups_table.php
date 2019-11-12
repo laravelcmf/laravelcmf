1b17572292c182e50706c59d2677570382367884
JeffreyBool <1402992668@qq.com>
2019-11-12 23:55:31 +0800

    增加权限组迁移文件

---
+++ database/migrations/2019_11_12_155138_create_permission_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->comment('权限组');
+            $table->smallInteger('sort')->default(0)->comment('排序');
+            $table->unsignedTinyInteger('status')->default(1)->comment('状态，1正常 2禁止 3删除');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_groups');
+    }
+}
