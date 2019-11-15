882478a0aefa183c094ab432f8efd6e30125eaf0
JeffreyBool <1402992668@qq.com>
2019-11-15 21:26:03 +0800

    add model menu

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,51 +2,9 @@
 
 namespace App\Models;
 
-/**
- * App\Models\Menu
- *
- * @property int $id
- * @property string|null $name 名称
- * @property int $parent_id 父级ID
- * @property string|null $icon 图标
- * @property string|null $path 路径
- * @property int $is_link 是否链接：0否，1是
- * @property int $sort 排序
- * @property int $status 状态，1正常 2禁止 3删除
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model statusDeleted()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model statusHidden()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model statusNormal()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereIcon($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereIsLink($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereParentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu wherePath($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereSort($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Menu whereUpdatedAt($value)
- * @mixin \Eloquent
- */
+use Illuminate\Database\Eloquent\Model;
+
 class Menu extends Model
 {
-    /**
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'parent_id',
-        'icon',
-        'path',
-        'is_link',
-        'sort',
-        'status',
-    ];
+    //
 }
---
+++ database/migrations/2019_11_15_131234_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('parent_id')->nullable()->comment('父级ID');
+            $table->string('parent_path')->nullable()->comment('父级路径');
+            $table->uuid('record_id')->comment('记录ID');
+            $table->string('name')->comment('菜单名称');
+            $table->unsignedInteger('sequence')->default(10000)->comment('排序值');
+            $table->string('icon')->nullable()->comment('图标');
+            $table->string('router')->nullable()->comment('访问路由');
+            $table->unsignedTinyInteger('hidden')->default(0)->comment('隐藏菜单: 0:不隐藏, 1:隐藏');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
