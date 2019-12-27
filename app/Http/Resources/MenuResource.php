7ee64407f1619ea84c53c019674d5997af58fbc5
JeffreyBool <1402992668@qq.com>
2019-12-27 19:59:11 +0800

    fix bug

--- app/Http/Resources/MenuResource.php
+++ app/Http/Resources/MenuResource.php
@@ -14,6 +14,7 @@ class MenuResource extends JsonResource
     public function toArray($request)
     {
         $data = parent::toArray($request);
+        $data['path'] = $data['path'] ? $data['path'] : '';
         $data['actions'] = new MenuActionResource($this->whenLoaded('actions'));
         $data['resources'] = new MenuResourceResource($this->whenLoaded('resources'));
         return $data;
